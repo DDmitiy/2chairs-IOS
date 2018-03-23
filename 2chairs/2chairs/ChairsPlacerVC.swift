fb90cdf9c190ca53e97667441795eff7e802e41b
SergLeontiev <translatertrans@gmail.com>
2018-03-24 01:07:11 +0300

    SLAVA UKRAINE!

---
+++ 2chairs/2chairs/ChairsPlacerVC.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  2chairs
+//
+//  Created by Somebody on 23.03.2018.
+//  Copyright © 2018 OttersTeam. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ChairsPlacerVC: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    var nodeModel : SCNNode!
+    let nodeName = "box"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        sceneView.antialiasingMode = .multisampling4X
+        let scene = SCNScene()
+        sceneView.scene = scene
+        let modelScene = SCNScene(named: "art.scnassets/box.dae")!
+        nodeModel =  modelScene.rootNode.childNode(
+            withName: nodeName, recursively: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let location = touches.first!.location(in: sceneView)
+        
+        // Let's test if a 3D Object was touch
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        
+        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+        
+        if let hit = hitResults.first {
+            if let node = getParent(hit.node) {
+                node.removeFromParentNode()
+                return
+            }
+        }
+        
+        // No object was touch? Try feature points
+        let hitResultsFeaturePoints: [ARHitTestResult]  = sceneView.hitTest(location, types: .featurePoint)
+        
+        if let hit = hitResultsFeaturePoints.first {
+            
+            // Get the rotation matrix of the camera
+            let rotate = simd_float4x4(SCNMatrix4MakeRotation(sceneView.session.currentFrame!.camera.eulerAngles.y, 0, 1, 0))
+            
+            // Combine the matrices
+            let finalTransform = simd_mul(hit.worldTransform, rotate)
+            sceneView.session.add(anchor: ARAnchor(transform: finalTransform))
+            //sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+        }
+        
+    }
+    
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+                
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+            }
+        }
+    }
+}
